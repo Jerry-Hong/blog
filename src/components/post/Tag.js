b8f5cea10848ff0bdc082d6f61bdec9233bf18df
JerryHong <s6323859@hotmail.com>
2019-04-22 02:17:14 +0800

    feat: adjust tag style

--- src/components/post/Tag.js
+++ src/components/post/Tag.js
@@ -1,33 +1,53 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Tag as TagIcon } from 'react-feather';
 import { Link } from 'gatsby';
 import { kebabCase } from '../../utils/ramdaExtention';
 
+const Content = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  margin-top: 3em;
+  margin-bottom: 2.5em;
+`;
+
 const TagListContent = styled.ul`
   display: flex;
+  flex-wrap: wrap;
   list-style: none;
   padding: 0;
-  margin-top: 3em;
-  align-items: center;
-
-  &::before {
-    content: 'Tags: ';
-    margin-right: 10px;
-  }
 `;
 
 const Tag = styled.li`
-  margin-right: 10px;
+  border: 1px solid var(--link);
+  border-radius: 3px;
+  margin-bottom: 1em;
+  margin-right: 1em;
+  padding: 0 3px;
+`;
+
+const Title = styled.span`
+  display: flex;
+  align-items: center;
+  font-weight: 500;
+  font-size: 1.1em;
 `;
 
 export const TagList = ({ tags }) =>
   tags &&
   tags.length && (
-    <TagListContent>
-      {tags.map(tag => (
-        <Tag key={`${tag}tag`}>
-          <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-        </Tag>
-      ))}
-    </TagListContent>
+    <Content>
+      <Title>
+        <TagIcon size={20} style={{ marginRight: 5 }} />
+        Tags
+      </Title>
+      <TagListContent>
+        {tags.map(tag => (
+          <Tag key={`${tag}tag`}>
+            <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+          </Tag>
+        ))}
+      </TagListContent>
+    </Content>
   );
